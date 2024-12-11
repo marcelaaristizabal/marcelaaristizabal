b22da784c6844b55a3c89c46901081e4cd986b6b
marcelaaristizabal <aristizabal.marcelasofia@gmail.com>
2024-12-11 14:45:51 -0500

    fot anidado

---
+++ curso-py/control-flujo/07-while.py
@@ -0,0 +1,13 @@
+# numero = 1
+
+# while numero < 100:
+#     print (numero)
+#     numero *= 2
+
+comando = ""
+
+while True:
+    comando = input("$: ")
+    print(comando)
+    if comando.lower() == "salir":
+        break
