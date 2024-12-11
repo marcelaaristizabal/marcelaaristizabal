91bee14abd7bf322ac21f0c4c222343ae7e08ea9
marcelaaristizabal <aristizabal.marcelasofia@gmail.com>
2024-12-11 14:31:39 -0500

    ciclo for

--- curso-py/control-flujo/06-for.py
+++ curso-py/control-flujo/06-for.py
@@ -1,2 +1,12 @@
+buscar = 10
+
 for numero in range(5):
     print (numero)
+    if numero == buscar:
+        print ("Encontrado", buscar)
+        break # Termina el ciclo
+else:
+    print ("No encontrado el número buscado")
+
+for char in "Ultimate Python":
+    print (char)
